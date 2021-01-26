d3efd06591f2751b1ed170eb3de4b7c9b8dc745e
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-01-26 14:21:28 +0300

    Update havadurumu.js

--- komutlar/havadurumu.js
+++ komutlar/havadurumu.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const weather = require("weather-js");
-
+ 
 exports.run = (client, message, args) => {
   weather.find({ search: args.join(" "), degreeType: "C" }, function(
     err,
     result
   ) {
-    if (err) message.channel.send(err);
+   
     if (result === undefined || result.length === 0) {
       message.channel.send(
         new Discord.MessageEmbed()
@@ -19,12 +19,12 @@ exports.run = (client, message, args) => {
     var current = result[0].current;
     var location = result[0].location;
 
-    const plasmic = new Discord.MessageEmbed()
+    const motion = new Discord.MessageEmbed()
 
       .setDescription(`**${current.skytext}**`)
       .setAuthor(`${current.observationpoint} için hava durumu`)
       .setThumbnail(current.imageUrl)
-      .setColor("RANDOM")
+      .setColor(0x00ae86)
       .addField("● Zaman Dilimi", `UTC${location.timezone}`, true)
       .addField("● Derece Türü", location.degreetype, true)
       .addField("● Sıcaklık", `${current.temperature} Derece`, true)
@@ -32,19 +32,19 @@ exports.run = (client, message, args) => {
       .addField("● Rüzgar", current.winddisplay, true)
       .addField("● Nem", `${current.humidity}%`, true);
 
-    message.channel.send({ plasmic });
+    message.channel.send(motion);
   });
 };
 
-exports.conf = {
+exports.conf = { 
   enabled: true,
   guildOnly: false,
-  aliases: ["havadurumu"],
+  aliases: ["YEDEK KOMUT1", "YEDEK KOMUT2"],
   permLevel: "0"
 };
 
 exports.help = {
-  name: "havadurumu",
+  name: "hava-durumu",
   description: "hava durumunu gösterir",
   usage: "havadurumu"
 };
