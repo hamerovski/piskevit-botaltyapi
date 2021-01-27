d5dd1f2e84f3f6803541c3799506f4bc46c4b72d
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-01-27 17:29:45 +0300

    Update yks-sayaç.js

--- komutlar/yks-sayaç.js
+++ komutlar/yks-sayaç.js
@@ -6,10 +6,13 @@ const virus = new Discord.MessageEmbed()
 
       .setAuthor("YKS SAYAÇ")
       .setColor("RANDOM")
-       .setFooter("Piskevit")
-      .setTimestamp()
-      .setDescription("")
-      .setImage('');
+      function prepareFrame() {
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "http://google.com/");
+        ifrm.style.width = "640px";
+        ifrm.style.height = "480px";
+        document.body.appendChild(ifrm);
+    }
 
 return message.channel.send(virus);
  
