8dadae2f455f3153cc76e05165082ddf0deefa0e
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-01-28 21:43:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,6 +150,73 @@ const countdown = (param) => {
 //-----------
 
 
+//----------- YKS GERİ SAYIM2 deadline-------------
+client.on("ready",async message => {
+  
+
+  
+   
+  var mesajid = "804420806977388545"
+  var kanalid = "804420750320205845"
+  var sunucuid = "768453225958998076"
+  var tarih = "June 26 2021 10:15:00 UTC+3" //İNGİLİZCE AY / GÜN / YIL / SAAT / DAKİKA / SANİYE 
+  var bitiş = "Zaman doldu koçum geçmiş olsun"
+  var yenilemearalığı = 5000 // milisaniye cinsinden EN AZ 5 SANİYE
+  
+
+
+  
+  
+let kanall = await client.channels.fetch(kanalid) 
+const countdownTextNow = (output) => {
+    return (date) => {
+        const ms = msCountdown(date)
+        const days = Math.floor( ms/(1000*60*60*24) );
+        const hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((ms % (1000 * 60)) / 1000);
+        return output(days,hours,minutes,seconds);
+    };
+}
+const msCountdown = date => Date.parse(date) - Date.parse(new Date());
+
+const countdown = (param) => {
+   
+     
+      
+        const cd = () => {
+            if (msCountdown(param.date) < 0) {
+            
+             client.channels.cache.get(kanalid).send(`${param.end}`).catch(err=>console.log(err));
+                return;
+            }
+       
+
+          kanall.messages.fetch(mesajid).then(x => {
+      x.edit(`${(countdownTextNow)(param.output)(param.date)}`)
+   
+   })
+           .then(a=>setTimeout(cd, param.timeout))
+                .catch(err=>console.log(err))
+ 
+        }; 
+        cd();
+    }
+
+  console.log(`${countdown({
+        date : tarih,
+        output: (d,h,m,s) => `${d}gün ${h}saat ${m}dakika ${s}saniye`,
+        end:  bitiş,
+        timeout: yenilemearalığı
+    })}`)
+  
+  
+  
+  
+})
+//-----------
+
+
 
 client.unload = command => {
     return new Promise((resolve, reject) => {
