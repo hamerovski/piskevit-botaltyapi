125e46242d7dd864dbbe394ffd6427888936e47d
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-01-29 12:33:22 +0300

    Update unban.js

--- komutlar/unban.js
+++ komutlar/unban.js
@@ -1,81 +1,18 @@
-const Discord = module.require("discord.js");
-const db = require('quick.db')
-module.exports.run = async (client, message, args) => {
-  
-
-  const permError = new Discord.RichEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 01 •')
-        .setDescription('```Bu Komutu Kullanmak İçin "Üyeleri Yasakla" Yetkisine Sahip Olmalısın```')
+if(command === "unban"){
+    if(!msg.member.hasPermission("BAN_MEMBERS")) {
+      return msg.channel.send(`**${msg.author.username}**, You do not have perms to unban someone`)
+    }
     
-  const userError = new Discord.RichEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 02 •')
-        .setDescription('```Yasağı kaldırmak için bir kullanıcı kimliği girmelisiniz /unban İD```')
-  
-  const userError2 = new Discord.RichEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 03 •')
-        .setDescription("```ID'de Harf Kullanılanamaz```")
-  
-  const userError3 = new Discord.RichEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 04 •')
-        .setDescription('```Bu Kullanıcı Yasaklanmamış```')
+    if(!msg.guild.me.hasPermission("BAN_MEMBERS")) {
+      return msg.channel.send(`**${msg.author.username}**, I do not have perms to unban someone`)
+    }
     
-  const levelError = new Discord.RichEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 05 •')
-        .setDescription('```Sizinle aynı veya daha yüksek bir role sahip bir üyenin yasağını kaldırmazsınız```')
-
-
-  if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send
-        (permError).then
-          (message.delete()).then
-            (msg => msg.delete(5000));
-  
-  let user = args[0];
-    if  (!user) return message.channel.send
-          (userError).catch(console.error).then
-            (message.delete()).then
-              (msg => msg.delete(5000));
-  
-  if  (isNaN(args[0])) return message.channel.send
-        (userError2).catch(console.error).then
-          (message.delete()).then
-            (msg => msg.delete(5000));
-
-  if  (user.roles.highest >= message.author.roles.highest) return message.channel.send
-          (levelError).then
-            (message.delete()).then
-              (msg => msg.delete(5000));
-  
-  
-  const banList = await message.guild.fetchBans();
-  
- // console.log(banList.map(s => s.users.id))
-  
-  if (!user.id === banList) return message.channel.send
-      (userError3).then
-        (message.delete()).then
-          (msg => msg.delete(5000));
-  
-  message.guild.unban(user);
-message.channel.send(`<@!${user}> Adlı Kullanıcının Yasağı Başarıyla Kaldırıldı.`)
-  
-  }
-
-  exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 2,
-        kategori: "Yetkili"
-
-  };
-
-  exports.help = {
-    name: 'unban',
-    description: 'İstediğiniz kişinin banını kaldırır.',
-    usage: 'unban'
-  };
+    let userID = args[0]
+      msg.guild.fetchBans().then(bans=> {
+      if(bans.size == 0) return 
+      let bUser = bans.find(b => b.user.id == userID)
+      if(!bUser) return
+      msg.guild.members.unban(bUser.user)
+})
+    
+  ;}
