10ffb3b3109f0449764bf93a1e307ecad8631ea7
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-01 00:38:24 +0300

    Update afk.js

--- komutlar/afk.js
+++ komutlar/afk.js
@@ -1,34 +1,62 @@
-const db = require("quick.db")
-const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const db = require("quick.db");
+exports.run = async (client, message, args) => {
+  const kisi = db.fetch(`afkid_${message.author.id}_${message.guild.id}`);
+  if (kisi) return;
+  const sebep = args[0];
+  if (!args[0]) {
+    let kullanıcı = message.guild.members.cache.get(message.author.id);
+    const b = kullanıcı.displayName;
+//ottomancode
+    await db.set(
+      `afkSebep_${message.author.id}_${message.guild.id}`,
+      "Sebep Girilmemiş"
+    ); //ottomancode
 
-exports.run = function(client, message, args) {
+    await db.set(
+      `afkid_${message.author.id}_${message.guild.id}`,
+      message.author.id
+    );
+    await db.set(`afkAd_${message.author.id}_${message.guild.id}`, b);
 
-  var user = message.author;
-  var sebep = args.slice(0).join("  ");
-  const sebep2 = new Discord.MessageEmbed()
-  .setDescription(` \`${message.author.username}\` **AFK moduna geçmek için bir sebep belirtmelisin.**`)
-  .setColor("#00ff88")
-  .setFooter(`AFK Sistemi.`, client.user.avatarURL());
-  if(!sebep) return message.channel.send(sebep2);
-  
-  db.set(`afk_${user.id}`, sebep);
-  db.set(`afk_süre_${user.id}`, Date.now());
-  const embed = new Discord.MessageEmbed()
-  .setDescription(` \`${user.username}\` **Başarıyla AFK moduna girdin.**`)
-  .setThumbnail(message.author.avatarURL())
-  .setColor("#00ff88")
-  .setFooter(`AFK Sistemi.`, client.user.avatarURL());
-  message.channel.send(embed)
-  
-};  
+    const ramo = await db.fetch(
+      `afkSebep_${message.author.id}_${message.guild.id}`
+    );
+//ottomancode
+
+   message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${kullanıcı} Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
+//ottomancode
+    message.member.setNickname(`[AFK] ` + b);
+  }
+  if (args[0]) {
+    let sebep = args.join(" ");
+    let kullanıcı = message.guild.members.cache.get(message.author.id);
+    const b = kullanıcı.displayName;
+    await db.set(`afkSebep_${message.author.id}_${message.guild.id}`, sebep);
+    await db.set(
+      `afkid_${message.author.id}_${message.guild.id}`,
+      message.author.id
+    );
+    await db.set(`afkAd_${message.author.id}_${message.guild.id}`, b);
+    const ramo = await db.fetch(
+      `afkSebep_${message.author.id}_${message.guild.id}`
+    );
+//ottomancode
+   message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${kullanıcı} Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
+
+    message.member.setNickname(`[AFK] ` + b);
+  }
+};
 
 exports.conf = {
-  enabled: true, 
-  guildOnly: true, 
+  enabled: true,
+  guildOnly: false,
   aliases: [],
-  permLevel: 0 
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'afk',
+  name: "afk",
+  description: "Afk Olmanızı Sağlar.",
+  usage: "afk / afk <sebep>"
 };
