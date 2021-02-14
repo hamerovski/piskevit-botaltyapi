838ddced14e5ee2fdd40b38d23d8bc4f675f1cb1
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-14 21:42:54 +0300

    Update canlidestek.js

--- komutlar/canlidestek.js
+++ komutlar/canlidestek.js
@@ -20,18 +20,18 @@ const emoji7 = message.client.emojis.cache.get('??');
         .addField('Hey', `?? Canlı Destek Çağrısı`)
         .setAuthor(`${message.author.tag} (${message.
                    
-                   author.id})`, `${message.author.avatarURL()}`)
+                   author.id})`, `${message.author.avatarURL()()}`)
         .setColor("RANDOM")
         .addField(`Bilgiler`, `**Sunucu**: ${message.guild.name} (${message.guild.id}) \n**Kanal**: ${message.channel.name} (${message.channel.id}) \n**Destek İsteyen**: ${message.author.tag} (${message.author.id}) \n**Destek Mesajı**: ${mesaj}`)
         .setFooter("Canlı Destek")
         .setTimestamp()
-      client.channels.get(destekKanal).send({
+      client.channels.cache.get(destekKanal).send({
         embed: embed
       });
-    const collector = client.channels.get(destekKanal).createCollector(message => message.content.startsWith(''), {
+    const collector = client.channels.cache.get(destekKanal).createMessageCollector(message => message.content.startsWith(''), {
       time: 0
     })
-    client.channels.get(destekKanal).send('?? Destek çagrısı bağlanmak için `katıl` yazınız. İptal Etmek İçin `kapat` yazınız.')
+    client.channels.cache.get(destekKanal).send('?? Destek çagrısı bağlanmak için `katıl` yazınız. İptal Etmek İçin `kapat` yazınız.')
     collector.on('message', (message) => {
       if (message.content === 'kapat') collector.stop('aborted')
       if (message.content === 'katıl') collector.stop('success')
@@ -40,11 +40,11 @@ const emoji7 = message.client.emojis.cache.get('??');
       if (reason === 'time') return message.reply('?? Çagrı zaman aşımına uğradı.')
       if (reason === 'aborted') {
         message.reply('Çağrı reddedildi.')
-        client.channels.get(destekKanal).send('?? Başarıyla çağrı reddedildi.')
+        client.channels.cache.get(destekKanal).send('?? Başarıyla çağrı reddedildi.')
       }
       if (reason === 'success') {
-        client.channels.get(destekKanal).send('?? Destek  alındı!')
-        client.channels.get(destekKanal).send('?? Destek kapatmak için `kapat` yazınız.')
+        client.channels.cache.get(destekKanal).send('?? Destek  alındı!')
+        client.channels.cache.get(destekKanal).send('?? Destek kapatmak için `kapat` yazınız.')
         chan.send(`${message.author}`)
         chan.send('Çağrınız bir destek yetkili tarafından alındı!')
         chan.send('En Yakın Zamanda Size Yardımcı Olacagız.')
@@ -56,12 +56,12 @@ const emoji7 = message.client.emojis.cache.get('??');
             if (message.author.id === client.user.id) return
             if (message.content.startsWith('kapat')) {
               message.channel.send('?? Çağrı Kapatıldı.')
-              if (message.channel.id === chan.id) client.channels.get(destekKanal).send('?? Çağrı karşı taraftan kapatıldı.')
+              if (message.channel.id === chan.id) client.channels.cache.get(destekKanal).send('?? Çağrı karşı taraftan kapatıldı.')
               if (message.channel.id === destekKanal) chan.send('?? Çağrı karşı taraftan kapatıldı.')
 
               return isEnabled = false
             }
-            if (message.channel.id === chan.id) client.channels.get(destekKanal).send(`?? **${message.author.tag}**: ${message.content}`)
+            if (message.channel.id === chan.id) client.channels.cache.get(destekKanal).send(`?? **${message.author.tag}**: ${message.content}`)
             if (message.channel.id === destekKanal) chan.send(`?? **${message.author.tag}**: ${message.content}`)
           }
           contact(client)
