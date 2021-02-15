55112b6fc176219f432e2070b6b8a96b7ee27e75
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-16 01:32:28 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,43 @@
-const chalk = require("chalk");
-const moment = require("moment");
-const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log([${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!);
-  console.log([${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${client.user.username} ismi ile giriş yapıldı!); 
+var oyun = [
+        "🔥 /yardım | Yardım Menüsü",
+        "⭐️ /napim | Lafı Çak!",
+        "⭐️ /koronatest | Test Yaptır!",
+        "⭐️ /döviz | Güncel Döviz Fiyatları",
+        "⭐️ /bitcoin | Bitcoin Fiyatları",
+        "🔥Hey, Sen Mükemmelsin🔥",
+        "⭐️ /tarot | Bir Kart Çek! "  
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+    client.on("ready", async () => {
+   log("Durum başarıyla ayarlandı")
+      client.user.setActivity(oyun[random], 
+        { url: 'https://twitch.tv/.',
+        type: 'STREAMING' }); 
+        }, 2 * 2500);
+  
+})
+  console.log(` `);
+  console.log(` `);
+  console.log(` `);
+  console.log(`+-----------------------------------------------------+`);
+  console.log(`|  Bilgi : Tüm Komutlar Yüklendi !                    |`);
   client.user.setStatus("dnd");
+  client.user.setActivity(`${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+  console.log(`|  Bilgi : Aktivite Ayarlandı                         |`);
+  console.log(`|  Bilgi : Şu An `+ client.guilds.size + ` Adet Sunucuda Aktif Durumdayım     |`);
+  console.log(`|  Şuanki Ismim : ` + ayarlar.isim + `                        |`);
+  console.log(`|  Id : ` + ayarlar.id + `                            |`);
+  console.log(`+-----------------------------------------------------+`);
+
 };
