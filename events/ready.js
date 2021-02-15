f92f5d479a507de1886b6fe49cb4fc987dcc2461
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-16 01:36:44 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,32 +7,25 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "🔥 /yardım | Yardım Menüsü",
-        "⭐️ /napim | Lafı Çak!",
-        "⭐️ /koronatest | Test Yaptır!",
-        "⭐️ /döviz | Güncel Döviz Fiyatları",
-        "⭐️ /bitcoin | Bitcoin Fiyatları",
-        "🔥Hey, Sen Mükemmelsin🔥",
-        "⭐️ /tarot | Bir Kart Çek! "  
+        "𝙌𝙐𝘽𝙄𝘾 𝘿𝙀𝙎𝙄𝙂𝙉™ 🔥",
+        "⭐️ Created By The 𓆩 ΛЯΞS 𓆪 ⭐️",
+        "⭐️ discord.gg/KFk7jtTsxd ⭐️"  
     ];
 
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-    client.on("ready", async () => {
-   log("Durum başarıyla ayarlandı")
-      client.user.setActivity(oyun[random], 
-        { url: 'https://twitch.tv/.',
-        type: 'STREAMING' }); 
+
+        client.user.setActivity(oyun[random], "𝙌𝙐𝘽𝙄𝘾 𝘿𝙀𝙎𝙄𝙂𝙉™" );
         }, 2 * 2500);
   
-})
   console.log(` `);
   console.log(` `);
   console.log(` `);
   console.log(`+-----------------------------------------------------+`);
   console.log(`|  Bilgi : Tüm Komutlar Yüklendi !                    |`);
   client.user.setStatus("dnd");
+  client.user.setActivity(`${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
   console.log(`|  Bilgi : Aktivite Ayarlandı                         |`);
   console.log(`|  Bilgi : Şu An `+ client.guilds.size + ` Adet Sunucuda Aktif Durumdayım     |`);
   console.log(`|  Şuanki Ismim : ` + ayarlar.isim + `                        |`);
