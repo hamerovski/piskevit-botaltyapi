1c75f80fb556f303c42c8d3abb70505df0f77e57
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-16 01:03:03 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,3 +9,5 @@ module.exports = client => {
   console.log([${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${client.user.username} ismi ile giriş yapıldı!); 
   client.user.setStatus("dnd");
 };
+
+
