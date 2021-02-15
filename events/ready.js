0524aee756440087d1d95aad5d2522d371fc7d7d
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-16 00:52:24 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,36 @@
-const chalk = require("chalk");
-const moment = require("moment");
-const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${client.user.username} ismi ile giriş yapıldı!`); 
-  client.user.setStatus("dnd");
+var oyun = [
+        "/yardım | Yardım menüsünü aç!",
+        "/korona | Korona istatistiklerini gör!",
+        "/tarot | Tarot kartı çek!",
+        "/bitcoin | BTC fiyatlarını listeler!",
+        "/napim | Lafı yapıştır! ",
+        "/özlüsöz | Efsane bir söz gönder!",
+        "/fal | Falında ne var?",
+        "/aşkölçer | Sana ne kadar aşık?"
+
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "https://twitch.tv/." );
+      }, 2 * 5000); //DEĞİŞME SÜRESİ
+  
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setActivity(`${prefix}yardım | Şu an ${client.channels.cache.size} adet kanala, ${client.guilds.cache.size} sunucuya ve ${client.users.cache.size} kullanıcıya hizmet veriliyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ${client.channels.cache.size} adet kanala, ${client.guilds.cache.size} sunucuya ve ${client.users.cache.size} kullanıcıya hizmet veriliyor!`);
+
+
 };
