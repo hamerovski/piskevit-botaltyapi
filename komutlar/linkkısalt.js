76b4cf76b11c4b349b3a8db230189604c611003f
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-18 22:37:09 +0300

    Update linkkısalt.js

--- komutlar/linkkısalt.js
+++ komutlar/linkkısalt.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) =&gt; {
   //if(message.author.id !== "534724915593609216") return;
   //message.delete();
   let botmessage = args.join(" ");
-  let embed = new Discord.RichEmbed()
+  let embed = new Discord.MessageEmbed()
     .setTitle(`Link Kısaltıcı`)
     .setDescription(
       `[Kısaltılan Link İçin Tıkla]( https://www.pnd.tl/st/?api=9d073993d7f2b7a84f17aa51a3f68e0333f90cc1&amp;url=${botmessage})`
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) =&gt; {
     .setFooter(
       "Uyarı: Kısaltılan Linklerdeki Reklamlardan Botumuz Sorumlu Değildir!"
     );
-  message.channel.sendEmbed(embed);
+  message.channel.send(embed);
 };
 exports.conf = {
   enabled: true,
