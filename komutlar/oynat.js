6c97444e94d721a55c1b0c457d697e79d8c8ddd6
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-19 04:32:49 +0300

    Create oynat.js

---
+++ komutlar/oynat.js
@@ -0,0 +1,26 @@
+onst Discord = require("discord.js")
+const fs = require("fs")
+const Fynx = "#36393e";
+const FynxDogru = "#22BF41";
+const FynxHata = "#f30707";
+
+module.exports.run = async (client, message, args) => {
+if(!message.member.voice.channel) return message.channel.send({embed: {color: FynxHata, description: `  | Müzik oynatabilmek için bir ses kanalında olmanız gerekmektedir!` }})
+if (!args[0]) return message.channel.send({embed: {color: FynxHata, description: `| Müzik oynatabilmek için aranacak kelime veyahut kelimeler giriniz!` }})  
+const SuAndaSarkiOynatilmaktadir = client.player.isPlaying(message.guild.id);
+if(SuAndaSarkiOynatilmaktadir){
+const sarki = await client.player.addToQueue(message.guild.id, args.join(" "));
+message.channel.send({embed: {color: FynxDogru, description: ` | \`${sarki.name}\` adlı müzik kuyruğa eklendi!` }})
+    } else {
+const sarki = await client.player.play(message.member.voice.channel, args.join(" "));
+ message.channel.send({embed: {color: FynxDogru, description: `  | Şu Anda Çalınan Müzik:\n\`${sarki.name}\`` }})
+sarki.queue.on('end', () => {
+message.channel.send({embed: {color: FynxHata, description: ` | Kuyruktaki tüm müzikler oynatıldı. Rache Music ses kanalından ayrılıyor!` }})
+    })
+    }    
+};
+
+module.exports.config = {
+    name: "oynat",
+    aliases: ["çal","play","o","p",]
+};
