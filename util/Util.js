58fda3561bbba79896f491a2fc1333b89e61186d
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-02-27 04:07:43 +0300

    Update Util.js

--- util/Util.js
+++ util/Util.js
@@ -1,6 +1,8 @@
 const request = require('node-superfetch');
 const crypto = require('crypto');
 const { IMGUR_KEY } = process.env;
+const { channelID } = member.voice;
+const botChannel = member.guild.voice.channelID;
 const yes = ['evet'];
 const no = ['hayır']
 
@@ -9,6 +11,33 @@ const deleteCommandMessages = function (msg, client) { // eslint-disable-line co
 	  return msg.delete();
 	}
   };
+
+
+  if (channelID !== botChannel) {
+    return;
+  }
+
+  return true;
+};
+
+let config;
+
+try {
+  config = require("../ayarlar.json");
+} catch (error) {
+  config = null;
+}
+
+exports.token = config ? config.token : process.env.token;
+exports.prefix = config ? config.prefix : process.env.prefix;
+exports.YOUTUBE_API_KEY = config ? config.YOUTUBE_API_KEY : process.env.YOUTUBE_API_KEY;
+exports.SOUNDCLOUD_CLIENT_ID = config ? config.SOUNDCLOUD_CLIENT_ID : process.env.SOUNDCLOUD_CLIENT_ID;
+exports.MAX_PLAYLIST_SIZE = config ? config.MAX_PLAYLIST_SIZE : process.env.MAX_PLAYLIST_SIZE;
+exports.PRUNING = config ? config.PRUNING : process.env.PRUNING;
+exports.STAY_TIME = config ? config.STAY_TIME : process.env.STAY_TIME;
+exports.DEFAULT_VOLUME = config ? config.DEFAULT_VOLUME: process.env.DEFAULT_VOLUME;
+exports.LOCALE = config ? config.LOCALE : process.env.LOCALE;
+
 
 class Util {
 	static wait(ms) {
