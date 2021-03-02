95971201a7289cfc75ed1aad700289d4381ca5c4
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-03-02 09:33:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,31 +98,6 @@ newState.setSelfDeaf(true);
 
 
 
-client.on("message", async(message) => {
-if(message.author.bot) return;
-let kanal = client.channels.cache.get('812854941445259304')
-if(message.channel.id !== kanal.id) return;
-
-kanal.messages.fetch({ limit: 2 }).then(messages => {
-let ilksay = parseInt(messages.map(a => a.content)[1])
-let sayi = Math.floor(ilksay + 1)
-let sonsay = parseInt(message.content)
-
-if(isNaN(sonsay)) return  message.channel.send('sayı yazmalısın..').then(msg => {
-                msg.delete({ timeout: 5000})
-                message.delete()
-            })
-
-if(sonsay !== sayi) return  message.channel.send('sayı düzenini bozmasan mı ne').then(msg => {
-                msg.delete({ timeout: 5000})
-                message.delete()
-            })
-                  })
-})
-
-
-
-
 //----------- YKS GERİ SAYIM -------------
 client.on("ready",async message => {
   
