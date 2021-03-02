095f07c317c8d211c9f73f35d905e64cf9c07014
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-03-02 06:50:20 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,15 +1,5 @@
-const Discord = require("discord.js");
 const ayarlar = require('../ayarlar.json');
-let talkedRecently = new Set();
-
 module.exports = message => {
-  if (talkedRecently.has(message.author.id)) {
-    return;
-  }
-  talkedRecently.add(message.author.id);
-    setTimeout(() => {
-    talkedRecently.delete(message.author.id);
-  }, 0);
   let client = message.client;
   if (message.author.bot) return;
   if (!message.content.startsWith(ayarlar.prefix)) return;
@@ -17,6 +7,15 @@ module.exports = message => {
   let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
   let cmd;
+  
+  if (!client.commands.has(command)) {
+    if (client.aliases.has(command)) {
+      cmd = client.commands.get(client.aliases.get(command));
+    } else {
+      message.channel.send(`Komutlarımda **${command}** adında bir komut bulamadım!`)
+    }
+  }
+  
   if (client.commands.has(command)) {
     cmd = client.commands.get(command);
   } else if (client.aliases.has(command)) {
