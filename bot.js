95b0fef2795c1c21f4a0ad77ef440fe1125eb894
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-03-13 14:28:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -264,6 +264,72 @@ const countdown = (param) => {
 })
 //---------------------------------------------------------------
 
+//----------- POLİSLİĞE GERİ SAYIM ---------------------------------------------------------------------------------------------------------------------
+client.on("ready",async message => {
+  
+
+  
+  
+  var mesajid = "820256857942786058"
+  var kanalid = "820254691807985674"
+  var sunucuid = "819111654784106546"
+  var tarih = "January 01 2022 00:00:00 UTC+3" //İNGİLİZCE AY / GÜN / YIL / SAAT / DAKİKA / SANİYE 
+  var bitiş = "Zaman doldu koçum geçmiş olsun"
+  var yenilemearalığı = 5000 // milisaniye cinsinden EN AZ 5 SANİYE
+  
+
+
+  
+  
+let kanall = await client.channels.fetch(kanalid) 
+const countdownTextNow = (output) => {
+    return (date) => {
+        const ms = msCountdown(date)
+        const days = Math.floor( ms/(1000*60*60*24) );
+        const hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((ms % (1000 * 60)) / 1000);
+        return output(days,hours,minutes,seconds);
+    };
+}
+const msCountdown = date => Date.parse(date) - Date.parse(new Date());
+
+const countdown = (param) => {
+   
+     
+      
+        const cd = () => {
+            if (msCountdown(param.date) < 0) {
+            
+             client.channels.cache.get(kanalid).send(`${param.end}`).catch(err=>console.log(err));
+                return;
+            }
+       
+
+          kanall.messages.fetch(mesajid).then(x => {
+      x.edit(`${(countdownTextNow)(param.output)(param.date)}`)
+   
+   })
+           .then(a=>setTimeout(cd, param.timeout))
+                .catch(err=>console.log(err))
+ 
+        }; 
+        cd();
+    }
+
+  console.log(`${countdown({
+        date : tarih,
+        output: (d,h,m,s) => `${d}gün ${h}saat ${m}dakika ${s}saniye`,
+        end:  bitiş,
+        timeout: yenilemearalığı
+    })}`)
+  
+  
+  
+  
+})
+//---------------------------------------------------------------
+
 
 
 
