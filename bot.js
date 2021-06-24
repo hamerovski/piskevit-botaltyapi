33274df382a239f4739cf412f76ee941ceb0a256
Hamza Erhan <65420815+hamerovski@users.noreply.github.com>
2021-06-24 13:35:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -248,73 +248,70 @@ client.on("message" , message => {
 
 
 
-//----------- YKS GERİ SAYIM ---------------------------------------------------------------------------------------------------------------------
-client.on("ready",async message => {
-  
+//----------- YKS GERİ SAYIM ---------------------------------------------------------------------------------------------------------------------client.on("ready",async message => {
 
   
   
-  var mesajid = "855730902855581717"
-  var kanalid = "855004498337726466"
-  var sunucuid = "855004497712644096"
-  var tarih = "June 26 2021 10:15:00 UTC+3" //İNGİLİZCE AY / GÜN / YIL / SAAT / DAKİKA / SANİYE 
-  var bitiş = "Zaman doldu koçum geçmiş olsun"
-  var yenilemearalığı = 5000 // milisaniye cinsinden EN AZ 5 SANİYE
-  
-
-
+    var mesajid = "855730902855581717"
+    var kanalid = "855004498337726466"
+    var sunucuid = "855004497712644096"
+    var tarih = "June 26 2021 10:15:00 UTC+3" //İNGİLİZCE AY / GÜN / YIL / SAAT / DAKİKA / SANİYE 
+    var bitiş = "Zaman doldu koçum geçmiş olsun"
+    var yenilemearalığı = 5000 // milisaniye cinsinden EN AZ 5 SANİYE
+    
   
   
-let kanall = await client.channels.fetch(kanalid) 
-const countdownTextNow = (output) => {
-    return (date) => {
-        const ms = msCountdown(date)
-        const days = Math.floor( ms/(1000*60*60*24) );
-        const hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((ms % (1000 * 60)) / 1000);
-        return output(days,hours,minutes,seconds);
-    };
-}
-const msCountdown = date => Date.parse(date) - Date.parse(new Date());
-
-const countdown = (param) => {
-   
+    
+    
+  let kanall = await client.channels.fetch(kanalid) 
+  const countdownTextNow = (output) => {
+      return (date) => {
+          const ms = msCountdown(date)
+          const days = Math.floor( ms/(1000*60*60*24) );
+          const hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((ms % (1000 * 60)) / 1000);
+          return output(days,hours,minutes,seconds);
+      };
+  }
+  const msCountdown = date => Date.parse(date) - Date.parse(new Date());
+  
+  const countdown = (param) => {
      
-      
-        const cd = () => {
-            if (msCountdown(param.date) < 0) {
-            
-             client.channels.cache.get(kanalid).send(`${param.end}`).catch(err=>console.log(err));
-                return;
-            }
        
-
-          kanall.messages.fetch(mesajid).then(x => {
-      x.edit(`${(countdownTextNow)(param.output)(param.date)}`)
+        
+          const cd = () => {
+              if (msCountdown(param.date) < 0) {
+              
+               client.channels.cache.get(kanalid).send(`${param.end}`).catch(err=>console.log(err));
+                  return;
+              }
+         
+  
+            kanall.messages.fetch(mesajid).then(x => {
+                x.edit(Discord.MessageEmbed().setColor("PURPLE").setDescription(`${(countdownTextNow)(param.output)(param.date)}`).setFooter("YKS SAYACI!"))
+     
+     })
+             .then(a=>setTimeout(cd, param.timeout))
+                  .catch(err=>console.log(err))
    
-   })
-           .then(a=>setTimeout(cd, param.timeout))
-                .catch(err=>console.log(err))
- 
-        }; 
-        cd();
-    }
-
-  console.log(`${countdown({
-        date : tarih,
-        output: (d,h,m,s) => `${d}gün ${h}saat ${m}dakika ${s}saniye`,
-        end:  bitiş,
-        timeout: yenilemearalığı
-    })}`)
+          }; 
+          cd();
+      }
   
-  
-  
-  
-})
-//---------------------------------------------------------------
-
-
+    console.log(`${countdown({
+          date : tarih,
+          output: (d,h,m,s) => `${d}gün ${h}saat ${m}dakika ${s}saniye`,
+          end:  bitiş,
+          timeout: yenilemearalığı
+      })}`)
+    
+    
+    
+    
+  }) 
+ kanall.messages.fetch(mesajid).then(x => {
+                x.edit(Discord.MessageEmbed().setColor("PURPLE").setDescription(`${(countdownTextNow)(param.output)(param.date)}`).setFooter("YKS SAYACI!"))
 //---------------------------------------------------------------
 //--------------------------------------
 
